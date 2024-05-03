80bcae672824e584a931c4754b48ce584223d63e
Chideradominic <ibeduchideradominic@gmail.com>
2024-05-03 03:16:25 +0100

    Finished

--- javascript.js
+++ javascript.js
@@ -1,5 +1,19 @@
+let color = "black";
+let click = false;
 document.addEventListener("DOMContentLoaded",function(){
   createBoard(16);
+  document.querySelector("body").addEventListener("click", function(e){
+    if(e.target.tagName !="BUTTON"){
+      click = !click;
+      let draw = document.querySelector("#draw");
+      if(click){
+        draw.innerHTML = "Now you can draw";
+      }
+      else{
+        draw.innerHTML= "You're not allowed to draw"
+      }
+    }
+  })
   let btnPopup = document.querySelector("#popup")
   btnPopup.addEventListener("click", function(){
     let size = getSize();
@@ -36,13 +50,14 @@ function getSize(){
   }
 }
 function colorDiv(){
-  if(color == "random"){
-    this.style.backgroundColor = `hsl(${Math.random() *360}, 100%, 50%)`;
-  }
-  else{
-    this.style.backgroundColor = "black"
-  }
-}
+  if(click){
+    if(color == "random"){
+      this.style.backgroundColor = `hsl(${Math.random() *360}, 100%, 50%)`;
+    }
+    else{
+      this.style.backgroundColor = "black"
+    }
+}}
 function setColor(colorChoice){
   color = colorChoice
 
